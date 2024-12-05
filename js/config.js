2853cbaa227464cd11516ab2563dd7a11698901d
ShikhGh <lghandehari@yahoo.com>
2024-12-05 15:45:53 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_oV5Jcdlq3', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4cd48am1859kbhpudhsngtq55l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_KwHbNupDV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6vmo5ptcig33b67d2ej7kntvva', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
